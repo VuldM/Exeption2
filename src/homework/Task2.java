4b3fe8634cbe93ed3af4af032690f863da4171f2
VoldemarM <aniairina@gmail.com>
2023-09-24 10:38:38 +0300

    change code Task2

--- src/homework/Task2.java
+++ src/homework/Task2.java
@@ -7,16 +7,12 @@ import java.util.ArrayList;
  */
 public class Task2 {
     public static void main(String[] args) {
-        double[] intArray = new double[10];
-        intArray[8] =1;
+        int[] intArray = new int[10];
+        intArray[8] = 1;
         int d = 0;
         try {
-            if(d == 0){
-                throw new ArithmeticException("деление на ноль");
-            }else {
-                double catchedRes1 = intArray[8] / d;
+               int catchedRes1 = intArray[8] / d;
                 System.out.println("catchedRes1 = " + catchedRes1);
-            }
         }
         catch (ArithmeticException e) {
             System.out.println("Catching exception: " + e);
